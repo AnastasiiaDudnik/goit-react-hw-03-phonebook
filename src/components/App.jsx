67a71a1cbcb9dc485403255dc302c02ae17f96a4
AnastasiiaDudnik <dudniknastya2000@gmail.com>
2023-03-09 22:38:59 +0200

    component did mount update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,14 +4,16 @@ import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 import { FormContainer } from './App.styled';
 
+const initialContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -43,7 +45,13 @@ export class App extends Component {
       this.setState({
         contacts: parsedContacs,
       });
+
+      return;
     }
+
+    this.setState({
+      contacts: initialContacts,
+    });
   }
 
   componentDidUpdate(prevState) {
